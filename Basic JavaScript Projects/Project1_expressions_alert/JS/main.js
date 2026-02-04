beeaa2821fc9266f41f09fcd2e81b502a5934d88
saifsyed0626 <saifsyed0626@gmail.com>
2026-02-04 13:17:13 -0500

    Basic JavaScripts Projects Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,35 @@
+window.alert("Hello, World!");
+    
+document.write('Hello, world!');
+
+var A = "This is a string";
+document.write(A);
+
+var message = "Hello! This is a string assignment.";
+window.alert(message);
+
+var message = "The teacher said, \"Don\'t forget to hand in your homework!\"";
+window.alert(message);
+
+document.write("\"Be who you are and say what you feel,"
+    + " because those who mind don\'t matter and those who matter don\'t mind.\""
+    + "-Dr.Suess");
+
+var B = "Concatenated" + " String"
+document.write(B)
+
+var firstname = "Joe", age = 12, favouriteColor = "Blue";
+document.write(firstname);
+
+3 + 3
+Document.write(3 + 3);
+
+function My_First_function() {               //Defining a function and naming it
+    var str = "This is the button text!";    //Defining a variable and giving it
+                                             //a string value
+    document.getElementById("Button_Text").innerHTML = str;  //Putting the value
+                                             //of the variable into the HTML elementFromPoint
+}                                            //with the "Button_Text" id
+
+
+
