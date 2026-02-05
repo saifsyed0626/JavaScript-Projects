42a4fab6aa442de95853336426de30944e726dd7
saifsyed0626 <saifsyed0626@gmail.com>
2026-02-05 13:58:31 -0500

    Project2_Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function () {
+    var str = "This is the button test!";
+    document.getElementById("Button_Test").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
