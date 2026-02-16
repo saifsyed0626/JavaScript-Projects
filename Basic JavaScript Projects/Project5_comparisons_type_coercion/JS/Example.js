ccf1f7980603900d1eb83e656ca364770015b3c6
saifsyed0626 <saifsyed0626@gmail.com>
2026-02-16 15:57:22 -0500

    Project5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -0,0 +1,8 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+document.getElementById("Test").innerHTML = isNaN('This is a string');
+
+document.getElementById("Test").innerHTML = isNaN('007');
+
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,52 @@
+document.write(typeof "Word");
+
+document.write("10" + 5);
+
+result1 = "5" + 3;
+console.log(result1);  // '53'
+
+result2 = "5" - 3;
+console.log(result2);  // 2
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+console.log(2 + 2);
+
+document.write(10 == 10);
+
+document.write(3 == 11);
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+
+X = 82;
+Y = "82";
+document.write(X === Y);
+
+A = "Magnus";
+B = "Magnus";
+document.write(A === B);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(5 > 10);
+}
+
